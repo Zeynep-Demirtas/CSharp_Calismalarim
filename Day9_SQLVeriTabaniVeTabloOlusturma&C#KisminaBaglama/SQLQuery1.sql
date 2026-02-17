c8414df6f2d32899aa0f21105a279a7fe71032ae
Zeynep Demirtaş <zeynepdemirtas610@gmail.com>
2026-02-18 01:04:05 +0300

    SQL Veri Tabanı ve Tablo Oluşturma & C# Kısmına Bağlama

---
+++ Day9_SQLVeriTabaniVeTabloOlusturma&C#KisminaBaglama/SQLQuery1.sql
@@ -0,0 +1,12 @@
+--Select Insert Update Delete
+
+--Select * From TblCategory
+--İki tire açıkklama satırı yapar
+--Select ProductName,ProductPrice From TblProduct
+--Yıldız yerine sadece istediğimiz verilerin de adını yazıp çağırabiliriz
+
+--Select * From TblProduct Where ProductName='Sütlaç'
+--Where if gibi kullanılır, tek tırnak kullanılır  çünkü SQL de her şey bir karakterdir 
+--F5 le sorgu çalıştırılır
+
+--Select * From TblProduct Where ProductPrice<80 And ProductStatus=1
